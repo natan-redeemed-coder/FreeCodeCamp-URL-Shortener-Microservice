0e711061e396656f022ba3e891033f8dcbb5917d
Natan <natan.redeemed.coder@gmail.com>
2026-06-22 16:33:00 +0000

    Pending changes exported from your codespace

--- index.js
+++ index.js
@@ -1,24 +1,73 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+const cors = require("cors");
+const dns = require('dns');
+require("dotenv").config();
+const express = require("express");
+const { url } = require("inspector");
+
+
 const app = express();
 
-// Basic Configuration
-const port = process.env.PORT || 3000;
+
+const urls = [];
+
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({extended: true}));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", (request, response) => {
+  response.sendFile(process.cwd() + "/views/index.html");
 });
 
-// Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.post('/api/shorturl', (request, response) => {
+  try {
+    const urlToShorten = new URL(request.body.url);
+    dns.lookup(
+      urlToShorten.hostname,
+      (error, address, family) => {
+        if (error === null) {
+          const indexOfUrl = urls.indexOf(request.body.url);
+          if (indexOfUrl === -1) {
+            urls.push(request.body.url);
+            response.json({
+              original_url: request.body.url,
+              short_url: urls.length - 1
+            });
+          } else {
+            response.json({
+              original_url: request.body.url,
+              short_url: indexOfUrl
+            });
+          }
+        } else {
+          response.json({
+            error: "invalid url"
+          });
+        }
+      }
+    );
+  } catch (error) {
+    response.json({
+      error: "invalid url"
+    });
+  }
 });
 
-app.listen(port, function() {
+app.get("/api/shorturl/:shorturl", (request, response) => {
+  const shorturl = Number(request.params.shorturl);
+  if (shorturl >= 0 && shorturl < urls.length) {
+    response.redirect(urls[shorturl]);
+  } else {
+    response.json({
+      error: "invalid shorturl"
+    });
+  }
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
